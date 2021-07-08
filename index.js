0ebe45926ed265eb30c0ef278e6bdaed8cd27e9a
The Alugalug Cat <maximedu57600@gmail.com>
2021-07-08 03:30:36 +0200

    Add files via upload
    Ajouts ligne Commentaire

--- index.js
+++ index.js
@@ -1,44 +1,67 @@
 const Discord = require("discord.js")
 const client = new Discord.Client() 
-const token_file = require("./token_file.json") 
-const ref_jeanma = [ 
-    "Chuis pas Président, Tu peux manger HALAL",
-    "Ta vision d'la France ? J'adère !",
-    "N'est-ce-Pas", "Jean-Ma Nakamura",
-    "Marine préfère poupou à papa",
-    "Wallah mais je déconne !",
-    "C'est pas comme ça qu'on fait les choses !",
-    "Thermostat 4 baby tu crames SAH !",
-    "Oh Djadjeanne !",
-    "Y'a pas moyen, Djadjeanne !",
-    "Y'a de la place, Y'a de la place, Y'a de la place, Pour ta ..., Pour ta ..., Pour ta ...",
-    "Train première place pour ..., et une escale a ...",
-    "il est tombé du mirador",
-    "Jeanne au secours",
-    "Sarkozy, c'est son saltimbanque de père",
-    "Je vais te faire courir moi tu vas voir, rouquin...",
-    "LCI c'est mes ennemies",
-    "J'te renvoie dans ton PI",
-    "Yallah famille no mali",
-    "J'emploie souvent la negation",
-    "ce n'est qu'un point de détail !"
-    ]
+const Gamedig = require('gamedig');
+// change the prefix to whatever you want
+const prefix = ';'
+
 client.on('ready', () => {
-    console.log("Amogusus")
+    console.log("coucou")
 });
 
 client.on("message", (msg) => {
-    if(msg.channel.type === 'dm') return ;
-    if(msg.author.id === '862023408329555968') return ;
-    const ref_choisie = ref_jeanma[Math.floor(Math.random() * ref_jeanma.length - 1)]
-    const rdm_walla = Math.floor(Math.random() * 20)
-    if(rdm_walla === 4){
-        msg.channel.send(ref_choisie);
+    if(msg.content.startsWith(`${prefix}server`)){
+        const jaaju = msg.content.split(' ')
+        if(jaaju.length !== 3) return msg.reply(`correct use of the command : ${prefix}server game ip_adress \n Example : ${prefix}server garrysmod ip_adress`)
+        
+        Gamedig.query({
+            type: `${jaaju[1].toString().toLowerCase()}`,
+            host: `${jaaju[2].toString().toLowerCase()}`
+        }).then((state) => {
+        if(jaaju[1] === 'garrysmod'){
+            const gmod = new Discord.MessageEmbed()
+            .setColor('#08BCFD')
+            .setTitle('Bot created by Raiden & Alugalug Cat')
+            .setDescription('Here is the result of your search about ' + `${jaaju[2]}` + ' on the game ' + `${jaaju[1]}` + ' !')
+            .setThumbnail('https://i.ibb.co/yP0cD9F/dd28ed7cc40b4970812f3ab2552773ac.webp')
+            .setTimestamp()   
+            .setAuthor('Server informations')
+            .setURL('https://github.com/Raidened/RetrieveGameServerInfo_Bot')
+            .addFields(
+                  { name: 'Name of the server: ', value: (state.name) },
+                  { name: 'Map information: ', value: (state.map) },
+                  { name: 'Password: ', value: (state.password) },
+                  { name: 'Server IP: ', value: (state.connect) },
+                  { name: 'Actual players: ', value: (state.players.length + '/' + state.maxplayers)},
+                  { name: 'Ping: ', value: (state.ping)}    
+            )
+            return msg.channel.send(gmod);
+        }
+        if(jaaju[1] !== 'garrysmod'){
+            const autres = new Discord.MessageEmbed()
+            .setColor('#08BCFD')
+            .setTitle('Bot created by Raiden & Alugalug Cat')
+            .setDescription('Here is the result of your search about ' + `${jaaju[2]}` + ' on the game ' + `${jaaju[1]}` + ' !')
+            .setThumbnail('https://i.ibb.co/yP0cD9F/dd28ed7cc40b4970812f3ab2552773ac.webp')
+            .setTimestamp()   
+            .setAuthor('Server informations')
+            .setURL('https://github.com/Raidened/RetrieveGameServerInfo_Bot')
+            .addFields(
+                  { name: 'Name of the server: ', value: (state.name) },
+                  { name: 'Server IP: ', value: (state.connect) },
+                  { name: 'Actual players: ', value: (state.players.length + '/' + state.maxplayers)},
+                  { name: 'Ping: ', value: (state.ping)}
+            )
+                msg.channel.send(autres);
+        }
+        }).catch((error) => {
+            msg.reply(`the server is offline or you're using the command in the wrong way \n Correct use of the command : ${prefix}server game ip_adress \n Example : ${prefix}server garrysmod ip_adress`)
+        }); 
+        
+        console.log(jaaju.length)
+
+        // Bot was created by Raiden & The Alugalug Cat for support contact us !!! https://github.com/Raidened/RetrieveGameServerInfo_Bot/issues
     }
 })
 
-
-
-
-client.login(token_file.Token)
-
+// Your token here
+client.login("YourSuperSecretImpostorToken")
